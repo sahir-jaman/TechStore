43a826a86675efd515dae3615ed51e51caff04f2
sahir jaman <55667189+sahir-jaman@users.noreply.github.com>
2024-06-25 11:19:44 +0600

    added cart functionality perfectly

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from home.views import *
 from products.views import *
+from cart.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
